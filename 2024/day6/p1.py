50f26ecfa2298cf9f8361d76b59179b2c9880568
Aryan <vasudevan.aryan@gmail.com>
2024-12-06 22:38:12 -0500

    Solve Day 6 Part 1.

---
+++ 2024/day6/p1.py
@@ -0,0 +1,45 @@
+seen = {}
+
+def move(grid, x, y, dir, inside):
+    while inside:
+        nextX = x + dir[0]
+        nextY = y + dir[1]
+
+        if nextY < 0 or nextY >= len(grid) or nextX < 0 or nextX >= len(grid[0]):
+            inside = False
+            return x, y, inside
+
+        if grid[nextY][nextX] == "#":
+            return x, y, inside
+
+        x = nextX
+        y = nextY
+
+        seen.add((x, y))
+
+    return x, y, inside
+
+
+
+grid = []
+with open("input.txt", "r") as file:
+    for line in file:
+        grid.append(list(line)[:-1])
+
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        if grid[row][col] != "^":
+            continue
+        else:
+            x, y = col, row
+            seen = {(x, y)}
+
+inside = True
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+curDir = 0
+
+while inside:
+    x, y, inside = move(grid, x, y, directions[curDir], inside)
+    curDir = (curDir + 1) % 4
+
+print(len(seen))
